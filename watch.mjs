53c7418075013ac0b65643d46744eba21cfd13e4
DanRoscigno <dan@roscigno.com>
2024-03-20 18:47:01 -0400

    stripped down to just changes
    Signed-off-by: DanRoscigno <dan@roscigno.com>

---
+++ watch.mjs
@@ -0,0 +1,16 @@
+import { watch } from "chokidar"
+
+const sourceEnglish = "./docs/en/"
+const destinationEnglish = "./docusaurus/docs/"
+
+const watcher = watch(sourceEnglish, {
+    persistent: true,
+    usePolling: true,
+})
+
+watcher
+    .on('add', path => console.log(`File ${path} has been added`))
+    .on('change', path => console.log(`File ${path} has been changed`))
+    .on('unlink', path => console.log(`File ${path} has been removed`));
+
+
